96f103c50c2686742822181519525c3f9f15b1ac
BradyBassett <bradydbassett@gmail.com>
2021-10-20 02:23:10 -0500

    added key

--- src/components/aboutMe/technologyList.tsx
+++ src/components/aboutMe/technologyList.tsx
@@ -7,9 +7,12 @@ const TechnologyList = () => {
         <section className="text-gray-400 my-4">
             <p>Here are some technologies I have used recently.</p>
             <ul className="grid grid-cols-3 col-span-1 grid-rows-2 row-span-1 grid-flow-col gap-2 pt-2 xl:mr-52">
-                {technologies.map((technology) => {
+                {technologies.map((technology, index) => {
                     return (
-                        <li className="flex text-sm w-auto lg:text-base">
+                        <li
+                            className="flex text-sm w-auto lg:text-base"
+                            key={index}
+                        >
                             <FaAngleRight className="mt-1 text-green-400" />
                             <p className="hover:text-green-400 transition-all ease-in duration-75">
                                 {technology}
