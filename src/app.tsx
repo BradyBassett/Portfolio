24498179c564d30af441c6f33756724f799828aa
BradyBassett <bradydbassett@gmail.com>
2021-10-20 15:38:08 -0500

    refactored index

--- src/app.tsx
+++ src/app.tsx
@@ -5,13 +5,9 @@ import AboutMe from "./components/aboutMe/aboutMe";
 import MyProjects from "./components/myProjects/myProjects";
 import ContactMe from "./components/contactMe/contactMe";
 
-let index = 0;
-
-const increment = (index: number): number => {
-    return index + 1;
-};
-
 const App: React.FC = () => {
+    let index = 0;
+
     return (
         <>
             <Navbar />
@@ -19,13 +15,13 @@ const App: React.FC = () => {
                 <Home />
                 <div className="bg-wave-1 w-full aspect-w-4 aspect-h-1 bg-no-repeat bg-cover bg-center"></div>
                 <div className="bg-layered-waves-1 w-full aspect-w-4 aspect-h-1 bg-no-repeat bg-cover bg-center"></div>
-                <AboutMe index={index} />
+                <AboutMe index={index++} />
                 <div className="bg-wave-2 w-full aspect-w-4 aspect-h-1 bg-no-repeat bg-cover bg-center"></div>
                 <div className="bg-layered-waves-2 w-full aspect-w-4 aspect-h-1 bg-no-repeat bg-cover bg-center "></div>
-                <MyProjects index={increment(index)} />
+                <MyProjects index={index++} />
                 <div className="bg-wave-3 w-full aspect-w-4 aspect-h-1 bg-no-repeat bg-cover bg-center"></div>
                 <div className="bg-layered-waves-3 w-full aspect-w-4 aspect-h-1 bg-no-repeat bg-cover bg-center "></div>
-                <ContactMe index={increment(index)} />
+                <ContactMe index={index++} />
             </main>
             <Footer />
         </>
