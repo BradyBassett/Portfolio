5399a71ae2b881a68ddfb623fd8bb967d7a20511
BradyBassett <bradydbassett@gmail.com>
2021-10-28 01:28:20 -0500

    replaced arc with image

--- src/components/home/canvas.tsx
+++ src/components/home/canvas.tsx
@@ -2,35 +2,29 @@ import React, { useRef, useEffect } from "react";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
 import Star from "./star";
 import { getRandomBias } from "../../utils/utils";
+import star from "../../utils/images/star.png";
 
 const Canvas: React.FC<{ className: string }> = ({ className }) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const starRef = useRef<HTMLImageElement>(null);
     const { width, height } = useWindowDimensions();
 
     useEffect(() => {
         const canvas = canvasRef.current!;
+        const star = starRef.current!;
         const ctx = canvas.getContext("2d")!;
         canvas.width = width;
         canvas.height = height;
         let animationFrameID: number;
         let starsArray: Star[] = [];
-        let numberOfStars = (canvas.height * canvas.width) / 2000;
+        // let numberOfStars = 1;
+        let numberOfStars = (canvas.height * canvas.width) / 2500;
 
         const initStars = (): void => {
             for (let i = 0; i < numberOfStars; i++) {
-                let radius = getRandomBias(0.1, 4, 0.1, 1);
-                let x = getRandomBias(
-                    radius * 2,
-                    window.innerWidth - radius * 2,
-                    window.innerWidth / 2,
-                    1
-                );
-                let y = getRandomBias(
-                    radius * 2,
-                    window.innerHeight - radius * 2,
-                    window.innerHeight / 2,
-                    1
-                );
+                let scale = getRandomBias(0.01, 0.8, 0.3, 1);
+                let width = Math.round(starRef.current!.width * scale);
+                let height = Math.round(starRef.current!.height * scale);
                 let velocityVariation = getRandomBias(
                     0.001,
                     0.9999,
@@ -50,10 +44,32 @@ const Canvas: React.FC<{ className: string }> = ({ className }) => {
                     velocityX *= -1;
                     velocityY *= -1;
                 }
+                let x = getRandomBias(
+                    0,
+                    window.innerWidth - 18,
+                    window.innerWidth / 2,
+                    1
+                );
+                let y = getRandomBias(
+                    0,
+                    window.innerHeight - 18,
+                    window.innerHeight / 2,
+                    1
+                );
                 let alpha = getRandomBias(0.1, 0.5, 0.3, 1);
 
                 starsArray.push(
-                    new Star(x, y, velocityX, velocityY, radius, alpha)
+                    new Star(
+                        x,
+                        y,
+                        velocityX,
+                        velocityY,
+                        scale,
+                        width,
+                        height,
+                        alpha,
+                        star
+                    )
                 );
             }
         };
@@ -70,12 +86,18 @@ const Canvas: React.FC<{ className: string }> = ({ className }) => {
             }
         };
         initStars();
+        starsArray[0].draw(ctx);
         render();
 
         return () => window.cancelAnimationFrame(animationFrameID);
     }, [width, height]);
 
-    return <canvas className={className} ref={canvasRef}></canvas>;
+    return (
+        <>
+            <img src={star} alt="star" ref={starRef} className="hidden" />
+            <canvas className={className} ref={canvasRef}></canvas>
+        </>
+    );
 };
 
 export default Canvas;
--- src/components/home/star.ts
+++ src/components/home/star.ts
@@ -3,57 +3,50 @@ import { getRandomBias } from "../../utils/utils";
 export default class Star {
     public color: string;
     public changeAlpha: number;
-    public constellationFlag: boolean;
 
     constructor(
         public x: number,
         public y: number,
         public velocityX: number,
         public velocityY: number,
-        public radius: number,
-        public alpha: number
+        public scale: number,
+        public width: number,
+        public height: number,
+        public alpha: number,
+        public star: HTMLImageElement
     ) {
         this.x = x;
         this.y = y;
         this.velocityX = velocityX;
         this.velocityY = velocityY;
-        this.radius = radius;
+        this.scale = scale;
         this.alpha = alpha;
+        this.star = star;
         this.color = "#FEFFE0";
         this.changeAlpha = getRandomBias(0.0003, 0.0008, 0.001, 1);
-        if (Math.random() <= 0.05) {
-            this.constellationFlag = true;
-        } else {
-            this.constellationFlag = false;
-        }
     }
 
     draw(ctx: CanvasRenderingContext2D) {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        ctx.fillStyle = this.color;
-        ctx.shadowBlur = this.radius * 2;
-        ctx.shadowColor = this.color;
         ctx.globalAlpha = this.alpha;
-        ctx.fill();
+        ctx.drawImage(this.star, this.x, this.y, this.width, this.height);
     }
 
     update(ctx: CanvasRenderingContext2D) {
         if (
-            this.x - this.radius > ctx.canvas.width ||
-            this.x + this.radius < 0 ||
-            this.y - this.radius > ctx.canvas.height ||
-            this.y + this.radius < 0
+            this.x - this.width > ctx.canvas.width ||
+            this.x + this.width * this.scale < 0 ||
+            this.y - this.height * this.scale > ctx.canvas.height ||
+            this.y + this.height * this.scale < 0
         ) {
             this.x = getRandomBias(
-                this.radius * 2,
-                ctx.canvas.width - this.radius * 2,
+                this.width,
+                ctx.canvas.width - this.width,
                 ctx.canvas.width - ctx.canvas.width / 4,
                 0.7
             );
             this.y = getRandomBias(
-                this.radius * 2,
-                ctx.canvas.height - this.radius * 2,
+                this.height,
+                ctx.canvas.height - this.height,
                 ctx.canvas.height / 2,
                 1
             );
@@ -67,14 +60,14 @@ export default class Star {
         if (this.alpha <= 0.001) {
             this.changeAlpha = Math.abs(this.changeAlpha);
             this.x = getRandomBias(
-                this.radius * 2,
-                ctx.canvas.width - this.radius * 2,
+                this.width,
+                ctx.canvas.width - this.width,
                 ctx.canvas.width - ctx.canvas.width / 6,
                 0.7
             );
             this.y = getRandomBias(
-                this.radius * 2,
-                ctx.canvas.height - this.radius * 2,
+                this.height,
+                ctx.canvas.height - this.height,
                 ctx.canvas.height / 2,
                 1
             );
