f154dd06c9d39627459eb197a964fbc5344483e7
BradyBassett <bradydbassett@gmail.com>
2021-11-09 22:30:09 -0600

    consolidated side elements

--- src/components/sideElement.tsx
+++ src/components/sideElement.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { socialIcons } from "../utils/data";
 
-const SideElement: React.FC<{ position: string }> = ({ position }) => {
+const SideElement: React.FC = () => {
     return (
         <div className="hidden lg:block text-gray-350 text-lg xl:text-xl">
             <div className="fixed flex-row left-11 bottom-0 z-10">
@@ -9,13 +9,11 @@ const SideElement: React.FC<{ position: string }> = ({ position }) => {
                     {socialIcons.map((iconElement, index) => {
                         const { icon, url } = iconElement;
                         return (
-                            <li className="pt-5" key={index}>
-                                <a
-                                    href={url}
-                                    className="hover:text-green-500 transition-all"
-                                >
-                                    {icon}
-                                </a>
+                            <li
+                                className="mt-5 hover:text-green-500 hover:transform hover:translate-x-1 transition-all"
+                                key={index}
+                            >
+                                <a href={url}>{icon}</a>
                             </li>
                         );
                     })}
@@ -25,7 +23,7 @@ const SideElement: React.FC<{ position: string }> = ({ position }) => {
             <div className="fixed right-10 bottom-32 pb-5 z-10">
                 <a
                     href="mailto:bradydbassett@gmail.com"
-                    className="vertical-rl hover:text-green-500 transition-all"
+                    className="vertical-rl hover:text-green-500 hover:animate-pulse transition-all"
                 >
                     bradydbassett@gmail.com
                 </a>
