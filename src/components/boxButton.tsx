aa59c74daa3b0ae793f8830aaadacbe894e55473
BradyBassett <bradydbassett@gmail.com>
2021-12-07 20:44:02 -0600

    converted from a to react link

--- src/components/boxButton.tsx
+++ src/components/boxButton.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import BoxButtonIProps from "../interfaces/boxButtonIProps";
 import { FaAngleDown } from "react-icons/fa";
+import { Link } from "react-scroll";
 
-const BoxButton: React.FC<BoxButtonIProps> = ({ className, text, goTo, scroll }) => {
+const BoxButton: React.FC<BoxButtonIProps> = ({
+    className,
+    text,
+    goTo,
+    scroll,
+}) => {
     const [isHovering, setIsHovering] = useState(false);
 
     return (
         <div className={`${className} flex justify-center`}>
-            <a
-                href={goTo}
+            <Link
+                activeClass="active"
+                to={goTo!}
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={1000}
+                className="text-2xl md:text-3xl text-green-400 cursor-pointer"
                 onMouseOver={() => setIsHovering(true)}
                 onMouseOut={() => setIsHovering(false)}
-                className="text-2xl md:text-3xl text-green-400"
             >
                 <p className="border-2 rounded-lg border-green-400 p-2 hover:bg-gray-700 hover:p-3 md:p-3 md:hover:p-4 transition-all ease-out duration-200">
                     {text}
@@ -19,7 +30,7 @@ const BoxButton: React.FC<BoxButtonIProps> = ({ className, text, goTo, scroll })
                 {isHovering && scroll && (
                     <FaAngleDown className="absolute left-0 right-0 mx-auto mt-3 animate-bounce" />
                 )}
-            </a>
+            </Link>
         </div>
     );
 };
